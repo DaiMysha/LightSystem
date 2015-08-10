e50dfa57b70e25332589f44ed7d07196bda0a382
DaiMysha <mlp.lemon.birdy@gmail.com>
2015-08-11 00:34:16 +0200

    [NOT WORKING] WIP adding actual rendering
    Doesn't work well yet
    
    But corrected an issue with AABB and debug rendering

--- src/LightSystem/SpotLight.cpp
+++ src/LightSystem/SpotLight.cpp
@@ -31,18 +31,64 @@ namespace LS {
     void SpotLight::render(const sf::IntRect& screen, sf::RenderTarget& target, sf::Shader* shader, const sf::RenderStates &states) {
         if(_intensity <= 0.0f) return;
 
+        debugRender(screen,target);
+
+        if(_renderTexture!=nullptr) {
+            //draw the sprite
+            //_sprite.setPosition(_center);
+            /*std::cout << "shape : " << _sprite.getPosition().x << ";" << _sprite.getPosition().y << std::endl;
+            target.draw(_sprite,states);
+
+            sf::CircleShape shape(_radius);
+            shape.setPosition(_center);
+            shape.setFillColor(_color);
+
+            target.draw(shape);*/
+        } else {
+            if(_spreadAngle == M_PIf*2.0f) {
+            } else {
+            }
+        }
+	}
+
+    void SpotLight::preRender(sf::Shader* shader) {
+        if(shader==nullptr) return; //oopsie, can't work without the shader
+        if(_renderTexture!=nullptr) delete _renderTexture;
+
+        _renderTexture = new sf::RenderTexture();
+        if(_renderTexture == nullptr) return; //couldn't allocate somehow, return
+        if(!_renderTexture->create(_radius,_radius)) return; //somehow texture failed, maybe too big, abort
+
+        //actual rendering code (finally!)
+        sf::VertexArray rect(sf::Quads,4);
+        rect[0].position = sf::Vector2f(0,0);
+        rect[1].position = sf::Vector2f(0,_radius);
+        rect[2].position = sf::Vector2f(_radius,_radius);
+        rect[3].position = sf::Vector2f(_radius,0);
+        rect[0].texCoords = sf::Vector2f(0.0,0.0);
+        rect[1].texCoords = sf::Vector2f(0.0,1.0);
+        rect[2].texCoords = sf::Vector2f(1.0,1.0);
+        rect[3].texCoords = sf::Vector2f(1.0,0.0);
+
         float r = _color.r * _intensity;
         float g = _color.g * _intensity;
         float b = _color.b * _intensity;
 
         sf::Color c(r,g,b,1.0);
 
-        if(_spreadAngle == M_PIf*2.0f) {
-        } else {
-        }
-	}
+        _renderTexture->clear();
+        //shader parameters
+        shader->setParameter(DMGDVT::LS::Light::LAS_PARAM_CENTER,sf::Vector2f(_radius/2.0f,_radius/2.0f));
+        shader->setParameter(DMGDVT::LS::Light::LAS_PARAM_RADIUS,_radius);
+        shader->setParameter(DMGDVT::LS::Light::LAS_PARAM_COLOR,c);
+        shader->setParameter(DMGDVT::LS::Light::LAS_PARAM_BLEED,_bleed);
+        shader->setParameter(DMGDVT::LS::Light::LAS_PARAM_LINEARITY,_linearity);
+        shader->setParameter(DMGDVT::LS::Light::LAS_PARAM_OUTLINE,true); //for debug
+        shader->setParameter(DMGDVT::LS::Light::LAS_PARAM_ISOMETRIC,_isometric); //for debug
 
-    void SpotLight::preRender(sf::Shader* shader) {
+        _renderTexture->draw(rect,shader);
+
+        _sprite.setTexture(_renderTexture->getTexture());
     }
 
     //keep this as debug option, not used for now at all
@@ -50,8 +96,9 @@ namespace LS {
         if(_intensity <= 0.0f) return;
 
         if(_spreadAngle == M_PIf*2.0f) {
-            sf::CircleShape shape(_size);
+            sf::CircleShape shape(_radius);
             shape.setPosition(_center);
+            shape.setOrigin(sf::Vector2f(1,1)*_radius);
             shape.setFillColor(_color);
 
             target.draw(shape);
@@ -89,9 +136,9 @@ namespace LS {
     void SpotLight::computeAABB() {
 
         if(_spreadAngle == M_PIf*2.0f) {
-            _aabb.left = -_radius/2.0f;
-            _aabb.top = -_radius/2.0f;
-            _aabb.width = _aabb.height = _radius;
+            _aabb.left = -_radius;
+            _aabb.top = -_radius;
+            _aabb.width = _aabb.height = _radius*2.0f;
         } else {
             // @TODO : move the rotation function to a real good DMGDVT::sfUtils folder
             sf::Vector2f v = DMUtils::sfml::rotate<float>(sf::Vector2f(0.0,_radius),_directionAngle);
