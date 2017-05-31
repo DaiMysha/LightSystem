750ab2226032cb907bd52e369391600c496c65b1
DaiMysha <mlp.lemon.birdy@gmail.com>
2017-05-31 11:00:46 +0200

    Corrected small mistake with intensity in shader

--- src/LightSystem/SpotLight.cpp
+++ src/LightSystem/SpotLight.cpp
@@ -399,7 +399,7 @@ namespace ls
         shader->setParameter(dm::ls::Light::LAS_PARAM_RADIUS,_radius);
         shader->setParameter(dm::ls::Light::LAS_PARAM_BLEED,_bleed);
         shader->setParameter(dm::ls::Light::LAS_PARAM_LINEARITY,_linearity);
-        shader->setParameter(dm::ls::Light::LAS_PARAM_INTENSITY,_intensity);
+        shader->setParameter(dm::ls::Light::LAS_PARAM_INTENSITY,std::abs(_intensity));
 
         sf::ConvexShape shape = _makeShape();
 
